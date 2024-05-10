a922db6585c5bc2bbc8eba5db40680be5c85a640
elian-WSL22H <elian.neppel@posteo.eu>
2024-05-10 20:01:29 +0900

    not really working eh

--- src/easy_robot_control/easy_robot_control/test.py
+++ src/easy_robot_control/easy_robot_control/test.py
@@ -9,14 +9,16 @@ import rclpy
 from rclpy.node import Node
 from rclpy.time import Duration
 
-class MoverNode(Node):
+from easy_robot_control.EliaNode import EliaNode
+
+class MoverNode(EliaNode):
     def __init__(self):
         super().__init__("node")  # type: ignore
         self.get_logger().warn("start")
         self.arr = np.zeros(2000, dtype=float)
-        for k in range(16):
+        for k in range(1):
             self.startup_timer = self.create_timer(
-                timer_period_sec=1/10,
+                timer_period_sec=1/100,
                 callback=self.tmr_callback,
             )
 
@@ -24,12 +26,13 @@ class MoverNode(Node):
         # return
         # self.get_logger().warn("tik")
         # self.startup_timer.cancel()
-        # wait = self.create_rate(1000)
-        # wait.sleep()
+        wait = self.create_rate(1000)
+        wait.sleep()
+        # self.destroy_rate(wait)
         # wait._timer.destroy()
-        # wait.destroy()
-        self.get_clock().sleep_for(Duration(seconds=1/1000))
-        self.arr += np.ones_like(self.arr)
+        wait.destroy()
+        # self.get_clock().sleep_for(Duration(seconds=1/1000))
+        # self.arr += np.ones_like(self.arr)
         # time.sleep(1/1000)
 
 def main(args=None):
