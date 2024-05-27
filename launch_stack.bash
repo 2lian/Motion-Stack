95433d2934e4f2cbfe1dcffd364655fe0e2ddd98
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-05-27 14:18:54 +0900

    Create launch_stack.bash

---
+++ launch_stack.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+cd "${ROS2_MOONBOT_WS}" || echo No folder shortcut, working in $PWD
+. "${ROS2_INSTALL_PATH}"/setup.bash || . /opt/ros/humble/setup.bash || echo Ros2 Humble not found
+# export ROS_DOMAIN_ID=58
+# rm -rf install
+# rm -rf build
+# . install/setup.bash
+colcon build --symlink-install
+. install/setup.bash
+export RCUTILS_CONSOLE_OUTPUT_FORMAT="{message}"
+export RCUTILS_COLORIZED_OUTPUT=1
+export NUMBA_CACHE_DIR="./numba_cache" # this will compile numba in a permanant file
+
+ros2 launch ./launch_stack_rviz.launch.py
+# ros2 run easy_robot_control test
