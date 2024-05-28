bb10c08f18255994ba89abc9d34a3b9e6c611584
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-05-28 10:52:51 +0900

    Delete build_messages.bash

--- build_messages.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# run this inside this folder
-cd "${ROS2_MOONBOT_WS}" || exit
-source "${ROS2_INSTALL_PATH}"/setup.bash
-colcon build --symlink-install --packages-select custom_messages
-. install/setup.bash
