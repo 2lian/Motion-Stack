c814c5dd55997b1a0ef209b94427bc1b41bde233
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-07-12 18:20:31 +0900

    rot disabled and ssh push

--- LAUNCH.bash
+++ LAUNCH.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 
+rm -rf ./build
+rm -rf ./log
+rm -rf ./install
 . launch_stack.bash
--- src/easy_robot_control/easy_robot_control/mover_node.py
+++ src/easy_robot_control/easy_robot_control/mover_node.py
@@ -341,31 +341,31 @@ class MoverNode(EliaNode):
         self.sleep(seconds=0.1)
         self.update_tip_pos()
         self.last_sent_target_set = self.live_target_set
-        r = False
-        # r = True
+        # r = False
+        r = True
         while r:
             # quat = qt.from_rotation_vector([0.3, 0, 0])
             # self.body_tfshift(np.array([0, 25, -25], dtype=float), quat)
             # self.body_tfshift(-np.array([0, 25, -25], dtype=float), 1/quat)
-            z_shift = 100
-            quat = qt.from_rotation_vector([0, 0.2, 0.0]) ** 1
+            z_shift = 100 * 2
+            quat = qt.from_rotation_vector([0, 0.0, 0.1]) ** 4
 
-            fl = []
-            for leg in range(self.NUMBER_OF_LEG - 0):
-                shift_msg = self.np2tfReq(np.array([50, 0, 0]), qt.one)
-                f = self.roll_client_arr[leg].call_async(shift_msg)
-                fl.append(f)
-            self.sleep(0.01)
+            # fl = []
+            # for leg in range(self.NUMBER_OF_LEG - 0):
+            #     shift_msg = self.np2tfReq(np.array([50, 0, 0]), qt.one)
+            #     f = self.roll_client_arr[leg].call_async(shift_msg)
+            #     fl.append(f)
+            # self.sleep(0.01)
 
             self.body_tfshift(np.array([0, 0, -z_shift], dtype=float), quat)
             self.body_tfshift(-np.array([0, 0, -z_shift], dtype=float), 1 / quat)
 
-            fl = []
-            for leg in range(self.NUMBER_OF_LEG - 0):
-                shift_msg = self.np2tfReq(np.array([0, 50, 0]), qt.one)
-                f = self.roll_client_arr[leg].call_async(shift_msg)
-                fl.append(f)
-            self.sleep(0.01)
+            # fl = []
+            # for leg in range(self.NUMBER_OF_LEG - 0):
+            #     shift_msg = self.np2tfReq(np.array([0, 50, 0]), qt.one)
+            #     f = self.roll_client_arr[leg].call_async(shift_msg)
+            #     fl.append(f)
+            # self.sleep(0.01)
 
             self.body_tfshift(np.array([0, 0, -z_shift], dtype=float), 1 / quat)
             self.body_tfshift(-np.array([0, 0, -z_shift], dtype=float), quat)
