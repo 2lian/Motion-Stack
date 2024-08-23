5bada081330d24fff4ce848d7123b817a2f9da41
elian-WSL22H <elian.neppel@posteo.eu>
2024-08-23 13:22:56 +0900

    gustavupdate

--- general_launch_settings.py
+++ general_launch_settings.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Tuple
 
 
 ROBOT_INDEX_TO_USE: int = 1  # corresponding to the ROBOTS below
-LAUNCH_UP_TO_LVL: int = 5
+LAUNCH_UP_TO_LVL: int = 3
 # node of levels up to (and including) this one will be launched
 
 INTERFACES: List[Tuple[str, str]] = [("rviz_basic", "rviz.launch.py")]
--- src/easy_robot_control/launch/launch_setting.py
+++ src/easy_robot_control/launch/launch_setting.py
@@ -11,7 +11,7 @@ from launch.substitutions import (
 # from ament_index_python.packages import get_package_share_directory
 
 std_movement_time = 2  # seconds
-movement_update_rate = 20.0  # Hz
+movement_update_rate = 30.0  # Hz
 number_of_legs = 4
 wheel_size = float(1000)
 
