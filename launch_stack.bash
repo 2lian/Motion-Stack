9be91309347be7fe5513db2886b79d02d942dec3
Elian NEPPEL <70491689+2lian@users.noreply.github.com>
2024-09-09 16:22:31 +0900

    Update launch_stack.bash

--- launch_stack.bash
+++ launch_stack.bash
@@ -2,7 +2,7 @@
 set -e -o pipefail
 
 cd "${ROS2_MOONBOT_WS}" || echo No folder shortcut, working in $PWD
-. "${ROS2_INSTALL_PATH}"/setup.bash || source /opt/ros/humble/setup.bash || echo Ros2 Humble not found, continuing
+. "${ROS2_INSTALL_PATH}"/setup.bash || source /opt/ros/humble/setup.bash || source /opt/ros/foxy/setup.bash || echo Ros2 not found, continuing
 # rm -rf install
 # rm -rf build
 # rm -rf log
