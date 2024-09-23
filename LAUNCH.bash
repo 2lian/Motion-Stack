2b2d9f3e40a609ee31b664dcfdc10f407ffd016e
elian-WSL22H <elian.neppel@posteo.eu>
2024-09-23 21:18:33 +0900

    shaping func

--- LAUNCH.bash
+++ LAUNCH.bash
@@ -4,4 +4,5 @@ set -e -o pipefail
 # rm -rf ./build
 # rm -rf ./log
 # rm -rf ./install
-. launch_stack.bash
+# . launch_stack.bash
+python3 ./src/easy_robot_control/easy_robot_control/trajectories.py
