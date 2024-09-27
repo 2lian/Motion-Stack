2afb1feb8af61da98cdff840970b1e805a8299de
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-09-27 18:08:15 +0900

    launch fix

--- src/easy_robot_control/launch/lvl_02_ik.py
+++ src/easy_robot_control/launch/lvl_02_ik.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch import LaunchDescription
@@ -9,25 +9,24 @@ from launch_setting import LEG_EE_LIST
 from launch_setting import params
 
 parameters: Dict[str, Any] = params.copy()
+paramList: List[Dict[str, Any]] = [parameters] * len(LEG_EE_LIST)
+for leg_index, ee_name in enumerate(LEG_EE_LIST):
+    p = paramList[leg_index]
+    p["leg_number"] = leg_index
+    p["end_effector_name"] = str(ee_name)
 
 ik_node_list = [
     Node(
         package=PACKAGE_NAME,
         namespace="",  # Default namespace
         executable="ik_heavy_node",
-        name=f"ik_{leg_index}",
+        name=f"""ik_{p["leg_number"]}""",
         arguments=["--ros-args", "--log-level", "info"],
         emulate_tty=True,
         output="screen",
-        parameters=[
-            parameters
-            | {
-                "leg_number": leg_index,
-                "end_effector_name": str(ee_name),
-            }
-        ],
+        parameters=[p],
     )
-    for leg_index, ee_name in enumerate(LEG_EE_LIST)
+    for p in paramList
 ]
 
 nodeList = ik_node_list
