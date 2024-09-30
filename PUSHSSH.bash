75083777a2bbe9febfafda70df3d48ca4b7c191b
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-09-30 18:11:31 +0900

    setup for  mH arm

--- PUSHSSH.bash
+++ PUSHSSH.bash
@@ -4,11 +4,12 @@ set -e -o pipefail
 export RCUTILS_CONSOLE_OUTPUT_FORMAT="{message}"
 export RCUTILS_COLORIZED_OUTPUT=1
 
-SSH_ADDRESS=${address_no6}
+# SSH_ADDRESS=${address_no6}
+SSH_ADDRESS=${address_no4}
 SOURCE_DIR="./"
 DESTINATION_DIR="${SSH_ADDRESS}:~/Moonbot-Motion-Stack/"
 
-rsync -av --progress --include='src/***' --include='*.*' --exclude='*' $SOURCE_DIR $DESTINATION_DIR
-rsync -av --progress --include='*.bash' --include='*.py' --exclude='*' $SOURCE_DIR $DESTINATION_DIR
+rsync -av --checksum --progress --exclude='*cache*' --include='src/***' --include='*.*' --exclude='*' $SOURCE_DIR $DESTINATION_DIR
+rsync -av --checksum --progress --include='*.bash' --include='*.py' --exclude='*' $SOURCE_DIR $DESTINATION_DIR
 
 
