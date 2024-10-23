d9b1f73a93a7600d731cbdfd22d68135a76c5893
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-10-23 21:16:33 +0900

    better doc

--- LAUNCH.bash
+++ LAUNCH.bash
@@ -3,11 +3,23 @@ set -e -o pipefail
 export M_LEG=ALL
 export USE_RVIZ=TRUE
 
-# rm -rf ./build
-# rm -rf ./log
-# rm -rf ./install
-# chmod +x ./launch_stack.bash
-. launch_stack.bash
+cd "${ROS2_MOONBOT_WS}" || echo No folder shortcut, working in $PWD
+. "${ROS2_INSTALL_PATH}"/setup.bash || source /opt/ros/humble/setup.bash || source /opt/ros/foxy/setup.bash || echo Ros2 not found for auto-sourcing, continuing
+# rm -rf install
+# rm -rf build
+# rm -rf log
+. install/setup.bash
+export RCUTILS_COLORIZED_OUTPUT=1
+colcon build --symlink-install --cmake-args -Wno-dev
+# colcon build --cmake-args -Wno-dev
+colcon test --packages-select easy_robot_control
+colcon test-result --verbose
+. install/setup.bash
+export RCUTILS_CONSOLE_OUTPUT_FORMAT="{message}"
+export NUMBA_CACHE_DIR="./numba_cache" # this will compile numba in a permanant file
+
+ros2 launch ./launch_stack_rviz.launch.py
+# ros2 run easy_robot_control test
 
 # cd ~/Moonbot-Motion-Stack/src/urdf_packer/urdf/hero_7dof 
 # xacro hero_dragon.xacro
