d9878daeeea1b6d08222bfc0e9207f9a53493ffc
Andras Polgar <azazdeaz@gmail.com>
2024-10-24 13:00:28 +0900

    Fix xhost settings and cleanup folder structure

--- docker/run.sh
+++ docker/run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+xhost +local:root
+
 docker run -it --rm \
     --name="moonbot-motion-stack" \
     --network=host --ipc=host \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="$(pwd):/root/moonbot" \
-    --volume="$(pwd)/docker/build:/root/moonbot/build" \
-    --volume="$(pwd)/docker/install:/root/moonbot/install" \
-    --volume="$(pwd)/docker/log:/root/moonbot/log" \
+    --volume="$(pwd):/moonbot" \
+    --volume="$(pwd)/docker/build:/moonbot/build" \
+    --volume="$(pwd)/docker/install:/moonbot/install" \
+    --volume="$(pwd)/docker/log:/moonbot/log" \
     moonbot
+
+xhost -local:root
