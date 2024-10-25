56e971a08b84bfcb416afb119277457e4b40817b
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-10-25 15:35:31 +0900

    all legs reactivated

--- LAUNCH.bash
+++ LAUNCH.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e -o pipefail
-export M_LEG=ALL
-export USE_RVIZ=TRUE
+export M_LEG=BASE
+export USE_RVIZ=FALSE
 
 cd "${ROS2_MOONBOT_WS}" || echo No folder shortcut, working in $PWD
 . "${ROS2_INSTALL_PATH}"/setup.bash || source /opt/ros/humble/setup.bash || source /opt/ros/foxy/setup.bash || echo Ros2 not found for auto-sourcing, continuing
 # rm -rf install
 # rm -rf build
 # rm -rf log
-. install/setup.bash
+# . install/setup.bash
 export RCUTILS_COLORIZED_OUTPUT=1
 colcon build --symlink-install --cmake-args -Wno-dev
 # colcon build --cmake-args -Wno-dev
