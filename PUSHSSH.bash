9757b5a20cb6fce3c07c6ae403c75ecd2dbef786
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-10-31 10:55:05 +0900

    fix

--- PUSHSSH.bash
+++ PUSHSSH.bash
@@ -6,11 +6,11 @@ export RCUTILS_CONSOLE_OUTPUT_FORMAT="{message}"
 export RCUTILS_COLORIZED_OUTPUT=1
 
 # SSH_ADDRESS=${address_no6}
-SSH_ADDRESS=${m1}
+SSH_ADDRESS=${m2}
 SOURCE_DIR="./"
 DESTINATION_DIR="${SSH_ADDRESS}:~/Moonbot-Motion-Stack/"
 
-# rsync -av --checksum --progress --exclude='./build/'--exclude='./install/'  --exclude='./log/' --exclude='*cache*' $SOURCE_DIR $DESTINATION_DIR
+rsync -av --checksum --progress --exclude='./build/'--exclude='./install/'  --exclude='./log/' --exclude='*cache*' $SOURCE_DIR $DESTINATION_DIR
 
 rsync -av --checksum --progress --exclude='*.vim' --exclude='*cache*' --include='src/***' --include='*.*' --exclude='*' $SOURCE_DIR $DESTINATION_DIR
 rsync -av --checksum --progress --exclude='*.vim' --include='*.bash' --include='*.py' --exclude='*' $SOURCE_DIR $DESTINATION_DIR
