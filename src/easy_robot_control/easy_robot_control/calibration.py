347d5899d3d6e2c632c5f2039498af74a5b7eb16
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2024-11-05 23:53:27 +0900

    angle recovery

--- src/easy_robot_control/easy_robot_control/calibration.py
+++ src/easy_robot_control/easy_robot_control/calibration.py
@@ -76,21 +76,24 @@ DIRECTION: Dict[str, int] = {
 }
 
 
-def update_csv(file_path, new_str: str, new_float: float) -> None:
+def update_csv(file_path, new_str: str, new_float: float) -> Tuple[str, Optional[str]]:
     rows = []
     str_found = False
+    file_path = path.expanduser(file_path)
+    row_save = None
 
     if not path.exists(file_path):
         # Create the file and write the header
         with open(file_path, mode="w", newline="") as file:
-            writer = csv.writer(file)   
+            writer = csv.writer(file)
 
     with open(file_path, mode="r") as file:
         reader = csv.reader(file)
         for row in reader:
             if row and row[0] == new_str:
                 # If the string is found, update the float value
-                row[1] = str(new_float)
+                row_save = row[1]
+                row[1] = str(new_float)  # useless ?
                 str_found = True
             rows.append(row)
 
@@ -103,8 +106,12 @@ def update_csv(file_path, new_str: str, new_float: float) -> None:
         writer = csv.writer(file)
         writer.writerows(rows)
 
+    return new_str, row_save
 
-def csv_to_dict(file_path):
+
+def csv_to_dict(file_path) -> Optional[Dict[str, float]]:
+    if not path.exists(file_path):
+        return None
     data_dict = {}
 
     # Open the CSV file in read mode
