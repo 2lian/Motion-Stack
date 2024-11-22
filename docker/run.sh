6877cac5be468f35088eb43191964189d147698d
Andras Polgar <azazdeaz@gmail.com>
2024-11-22 10:02:13 +0100

    Use nvidia isaac container base

--- docker/run.sh
+++ docker/run.sh
@@ -2,21 +2,16 @@
 
 xhost +local:root
 
-# Create Docker volumes if they don't already exist
-docker volume create moonbot_build
-docker volume create moonbot_install
-docker volume create moonbot_log
-
 docker run -it --rm \
     --name="moonbot-motion-stack" \
     --network=host --ipc=host \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="$(pwd):/moonbot" \
-    --volume="moonbot_build:/moonbot/build" \
-    --volume="moonbot_install:/moonbot/install" \
-    --volume="moonbot_log:/moonbot/log" \
+    --volume="$(pwd):/moonbot:ro" \
+    --mount type=tmpfs,destination=/moonbot/build \
+    --mount type=tmpfs,destination=/moonbot/install \
+    --mount type=tmpfs,destination=/moonbot/log \
     moonbot
 
 xhost -local:root
