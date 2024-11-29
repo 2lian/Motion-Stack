60c3f3579decf199a9fabdf80b63f03584c76e36
elian-WSL22H <elian.neppel@posteo.eu>
2024-11-30 01:22:53 +0900

    tests

--- LAUNCH.bash
+++ LAUNCH.bash
@@ -16,14 +16,14 @@ cd "${ROS2_MOONBOT_WS}" || echo No folder shortcut, working in $PWD
 export RCUTILS_COLORIZED_OUTPUT=1
 colcon build --symlink-install --cmake-args -Wno-dev
 # colcon build --cmake-args -Wno-dev
-# colcon test --packages-select easy_robot_control
-# colcon test-result --verbose
+colcon test --packages-select easy_robot_control ros2_m_hero_pkg rviz_basic
+colcon test-result --verbose
 . install/setup.bash
 export RCUTILS_CONSOLE_OUTPUT_FORMAT="{message}"
 export NUMBA_CACHE_DIR="./numba_cache" # this will compile numba in a permanant file
 
 # ros2 launch easy_robot_control moonbot_zero.launch.py
-ros2 launch ros2_m_hero_pkg hero_vehicle.launch.py
+# ros2 launch ros2_m_hero_pkg hero_vehicle.launch.py
 # ros2 launch moonbot_zero_tuto myrobot.launch.py
 # ros2 launch ./robot_launcher.launch.py
 # ros2 run easy_robot_control test
