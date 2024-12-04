c6262314137dec51485617c0bb1352bc31c2bd09
Agresj <ahresgg@gmail.com>
2024-12-04 19:30:29 +0900

    fix

--- launch_stack.bash
+++ launch_stack.bash
@@ -13,6 +13,7 @@ colcon build --cmake-args -Wno-dev
 colcon test --packages-select easy_robot_control
 colcon test-result --verbose
 . install/setup.bash
+. ~/Stack_Interface/install/setup.bash
 export RCUTILS_CONSOLE_OUTPUT_FORMAT="{message}"
 export NUMBA_CACHE_DIR="./numba_cache" # this will compile numba in a permanant file
 
