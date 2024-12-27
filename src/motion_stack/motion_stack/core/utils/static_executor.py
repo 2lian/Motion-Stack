4c75bfbd90e3be9557f7c62dc5a0b64a66b51ac8
elian-WSL22H <elian.neppel@posteo.eu>
2024-12-27 21:44:52 +0900

    wip

--- src/motion_stack/motion_stack/core/utils/static_executor.py
+++ src/motion_stack/motion_stack/core/utils/static_executor.py
@@ -1,7 +1,25 @@
 from time import time_ns
+from typing import Any, Dict, List
 
 from .printing import TCOL
 from .time import Time
+
+default_param = [
+    ("leg_number", int, 0),
+    ("std_movement_time", float, 0.5),
+    ("control_rate", float, 30),
+    ("mvmt_update_rate", float, 10),
+    ("ignore_limits", bool, False),
+    ("limit_margin", float, 0),
+    ("speed_mode", bool, False),
+    ("add_joints", List[str], [""]),
+    ("ignore_limits", bool, False),
+    ("start_coord", List[float], [0, 0, 0]),
+    # ("mirror_angles", bool, False),
+    ("urdf_path", str, ""),
+    ("start_effector_name", str, ""),
+    ("end_effector_name", str, ""),
+]
 
 
 class Spinner:
@@ -10,6 +28,7 @@ class Spinner:
     def warn(self) -> None: ...
     def info(self) -> None: ...
     def debug(self) -> None: ...
+    def get_parameter(self, name: str, value_type, default=None): ...
 
 
 class PythonSpinner(Spinner):
@@ -40,9 +59,10 @@ class FlexNode:
 
     def __init__(self, spinner: Spinner):
         self.spinner = spinner
+        self.__ms_param: Dict[str, Any]
 
-    def now(self):
-        self.spinner.now()
+    def now(self) -> Time:
+        return self.spinner.now()
 
     def error(self, *args):
         self.spinner.error(*args)
@@ -55,3 +75,15 @@ class FlexNode:
 
     def debug(self, *args):
         self.spinner.debug(*args)
+
+    @property
+    def ms_param(self):
+        if self.__make_ms_param is None:
+            self.__ms_param = self.__make_ms_param()
+        return self.__ms_param
+
+    def __make_ms_param(self) -> Dict[str, Any]:
+        params: Dict[str, Any] = {}
+        for p, t, v in default_param:
+            params[p] = self.spinner.get_parameter(p, t, v)
+        return params
