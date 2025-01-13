95fc30254cedd8a7cc3d434393b7d6df78bcef6b
Elian NEPPEL <70491689+hubble14567@users.noreply.github.com>
2025-01-13 13:52:10 +0900

    fallback colcon

--- launch_stack.bash
+++ launch_stack.bash
@@ -5,7 +5,7 @@ cd "${ROS2_MOONBOT_WS}" || echo No folder shortcut, working in $PWD
 . "${ROS2_INSTALL_PATH}"/setup.bash || source /opt/ros/humble/setup.bash || source /opt/ros/foxy/setup.bash || echo Ros2 not found for auto-sourcing, continuing
 export RCUTILS_COLORIZED_OUTPUT=1
 # colcon build --cmake-args -Wno-dev
-doit -n 8 build
+doit -n 8 build || colcon build --cmake-args -Wno-dev
 # colcon test --packages-select easy_robot_control
 # colcon test-result --verbose
 . install/setup.bash
