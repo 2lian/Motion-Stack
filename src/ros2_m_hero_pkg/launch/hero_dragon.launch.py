ac19f38b82540ee4111de29d46e7f6aa979bd98f
elian-WSL22H <elian.neppel@posteo.eu>
2025-01-24 10:50:23 +0900

    meshes repaired and urdf realigned

--- src/ros2_m_hero_pkg/launch/hero_dragon.launch.py
+++ src/ros2_m_hero_pkg/launch/hero_dragon.launch.py
@@ -12,22 +12,14 @@ MANIP_LIMB = 3
 BACK_WHEEL = 4
 FRONT_WHEEL = 2  # the one wheel with 2 limb connected
 
-leg_dict: Dict[int, Union[str, int]] = {  # leg number -> end effector
-    # 1: 0,
-    # 2: 0,
-    # 2: "wheel14_c45",
-    # 4: f"leg4gripper2_straight",
-    # 11: "11wheel_in",
-    # 12: "wheel12_in",
-    # 13: "13wheel_in",
-    # 14: "wheel14_in",
-}
+leg_dict: Dict[int, Union[str, int]] = {}  # leg number -> end effector
 leg_dict[BRIDGE_LIMB] = f"wheel1{BACK_WHEEL}_c45"
 leg_dict[MANIP_LIMB] = f"leg{MANIP_LIMB}gripper2_straight"
-leg_dict[10+BACK_WHEEL] = f"wheel1{BACK_WHEEL}_in"
-leg_dict[10+FRONT_WHEEL] = f"wheel1{FRONT_WHEEL}_in"
+leg_dict[10 + BACK_WHEEL] = f"wheel1{BACK_WHEEL}_in"
+leg_dict[10 + FRONT_WHEEL] = f"wheel1{FRONT_WHEEL}_in"
 
 builder = LevelBuilder(urdf_from_name(ROBOT_NAME), leg_dict)
+
 
 def generate_launch_description():
     return builder.make_description()
