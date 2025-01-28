b61a6ef7b38c67b80a47f7d3046595f2945d87fd
elian-WSL22H <elian.neppel@posteo.eu>
2025-01-28 17:54:20 +0900

    improved syncer

--- src/motion_stack/motion_stack/api/ros2/leg_api.py
+++ src/motion_stack/motion_stack/api/ros2/leg_api.py
@@ -1,14 +1,17 @@
 from collections import ChainMap
+from typing import Callable, Dict, List, Optional, Set, Tuple
 
 from rclpy.node import Node
 from rclpy.task import Future
 from sensor_msgs.msg import JointState
 
+from motion_stack.core.utils.joint_state import JState, impose_state
 import motion_stack.ros2.communication as comms
+import motion_stack.ros2.ros2_asyncio.ros2_asyncio as rao
 from motion_stack.ros2.utils.executor import error_catcher
 from motion_stack.ros2.utils.joint_state import publish_jstate, ros2js, ros2js_wrap
 
-from ..joint_syncer import *
+from ..joint_syncer import JointSyncer
 
 
 class JointHandler:
@@ -19,23 +22,66 @@ class JointHandler:
         self.limb_number = limb_number
         self.sub = node.create_subscription(
             comms.lvl1.output.joint_state.type,
-            f"leg{limb_number}/{comms.lvl1.output.joint_state.name}",
+            f"{comms.limb_ns(self.limb_number)}/{comms.lvl1.output.joint_state.name}",
             ros2js_wrap(self._update_state),
             10,
         )
         self.new_state_cbk: List[Callable[["JointHandler"],]] = []
         self.pub = node.create_publisher(
             comms.lvl1.input.joint_target.type,
-            f"leg{limb_number}/{comms.lvl1.input.joint_target.name}",
+            f"{comms.limb_ns(self.limb_number)}/{comms.lvl1.input.joint_target.name}",
             10,
         )
         self.advert = node.create_client(
             comms.lvl1.output.advertise.type,
-            f"leg{limb_number}/{comms.lvl1.output.advertise.name}",
+            f"{comms.limb_ns(self.limb_number)}/{comms.lvl1.output.advertise.name}",
         )
         self.ready = Future()
-        self.paired = Future()
-        self._update_tracked()
+        self._paired = Future()
+        self.ready_up()
+
+    def ready_up(self, tracked: Optional[Set[str]] = None) -> Tuple[Future, Future]:
+        timeout = 3
+        self.ready.cancel()
+        self._paired.cancel()
+        self.ready = Future()
+        self._paired = Future()
+
+        if tracked is not None:
+            self.tracked = tracked
+            self._paired.set_result(self.tracked)
+            return self.ready, self._paired
+
+        @error_catcher
+        def call_processingCBK(call):
+            if call.cancelled():
+                return
+            if self._paired.done():
+                return
+            # print("answer")
+            msg: JointState = call.result().js
+            js = ros2js(msg)
+
+            self.tracked = {js.name for js in js}
+            self._paired.set_result(self.tracked)
+
+        prev_call = Future()
+
+        @error_catcher
+        def tmrCBK():
+            if self._paired.done():
+                return
+            nonlocal prev_call
+            prev_call.cancel()
+            prev_call = self.advert.call_async(comms.lvl1.output.advertise.type.Request())
+            prev_call.add_done_callback(call_processingCBK)
+
+        # tmr = self._node.create_timer(
+        # timeout, lambda *_: rao.ensure_future(self._node, cbk())
+        # )
+        tmr = self._node.create_timer(timeout, tmrCBK)
+        self._paired.add_done_callback(lambda *_: self._node.destroy_timer(tmr))
+        return self.ready, self._paired
 
     def _update_tracked(self) -> Tuple[Future, Future]:
         out_fut = Future()
@@ -58,11 +104,11 @@ class JointHandler:
         call = self.advert.call_async(comms.lvl1.output.advertise.type.Request())
         call.add_done_callback(cbk)
 
-        self.paired.cancel()
-        self.paired = out_fut
+        self._paired.cancel()
+        self._paired = out_fut
         self.ready.cancel()
         self.ready = Future()
-        return self.paired, self.ready
+        return self._paired, self.ready
 
     @property
     def states(self) -> List[JState]:
@@ -80,9 +126,9 @@ class JointHandler:
                 for js in states
             }
         )
-        if not self.ready.done() and self.paired.done():
-            # print(set(self._states.keys()))
-            if set(self._states.keys()) >= self.tracked:
+        if not self.ready.done() and self._paired.done():
+            tracked_joint_data_available = set(self._states.keys()) >= self.tracked
+            if tracked_joint_data_available:
                 self.ready.set_result(True)
         for f in self.new_state_cbk:
             f(self)
--- src/motion_stack/motion_stack/ros2/default_node/trial.py
+++ src/motion_stack/motion_stack/ros2/default_node/trial.py
@@ -1,91 +1,85 @@
-import asyncio as ao
+import json
 
-import rclpy
-from rclpy.executors import SingleThreadedExecutor
 from rclpy.node import Node
-from rclpy.task import Future
 
 import motion_stack.ros2.ros2_asyncio.ros2_asyncio as rao
 from motion_stack.api.ros2.leg_api import JointHandler, JointSyncerRos
 from motion_stack.ros2.utils.executor import error_catcher, my_main
 
-import json
-
 
 class TestNode(Node):
     def __init__(self) -> None:
-        filepath = "/home/elian/Downloads/episodes.json"
-        f = open(filepath)
-        self.path = json.load(fp=f)
-        print(self.path[0]["action"]["arm_joint1"])
-        print(self.path[1]["action"]["arm_joint1"])
-        print(self.step(1))
+        JSON_PATH = "/home/elian/episodes.json"
+        self.JSON_TRAJECTORY = json.load(open(JSON_PATH))
+        self.LEG_NUM = 1
+
         super().__init__("test_node")
-        self.handlers = [JointHandler(self, l + 1) for l in [2]]
+        self.handlers = [JointHandler(self, l) for l in [self.LEG_NUM]]
         self.syncer = JointSyncerRos(self.handlers)
         self.create_timer(0.1, self.loop)
-        self.startTMR = self.create_timer(0.1, self.start)
+        self.startTMR = self.create_timer(0.1, self.startup)
         self.get_logger().info("init done")
 
-    async def update(self):
-        while 1:
-            l = [jh._update_tracked()[1] for jh in self.handlers]
-            try:
-                await rao.wait_for(self, rao.gather(self, *l), timeout_sec=2)
-                print(f"Joints ready.")
-                print([jh.tracked for jh in self.handlers])
-                return
-            except TimeoutError:
-                print("timedout")
+    async def ready_up(self):
+        l = [jh.ready for jh in self.handlers]
+        try:
+            await rao.wait_for(self, rao.gather(self, *l), timeout_sec=100)
+            print(f"Joints ready.")
+            strlist = "\n".join(
+                [f"limb {jh.limb_number}: {jh.tracked}" for jh in self.handlers]
+            )
+            print(f"Joints are:\n{strlist}")
+            return
+        except TimeoutError:
+            raise TimeoutError("Joint data unavailable after 100 sec")
 
-    def step(self, n: int):
-        data = self.path[n]["action"]
+    def json_step(self, n: int):
+        data = self.JSON_TRAJECTORY[n]["action"]
         remap = {
-            "arm_joint1": "leg3joint1",
-            "arm_joint2": "leg3joint2",
-            "arm_joint3": "leg3joint3",
-            "arm_joint4": "leg3joint4",
-            "arm_joint5": "leg3joint5",
-            "arm_joint6": "leg3joint6",
-            "arm_joint7": "leg3joint7",
+            "arm_joint1": f"leg{self.LEG_NUM}joint1",
+            "arm_joint2": f"leg{self.LEG_NUM}joint2",
+            "arm_joint3": f"leg{self.LEG_NUM}joint3",
+            "arm_joint4": f"leg{self.LEG_NUM}joint4",
+            "arm_joint5": f"leg{self.LEG_NUM}joint5",
+            "arm_joint6": f"leg{self.LEG_NUM}joint6",
+            "arm_joint7": f"leg{self.LEG_NUM}joint7",
         }
         off = {
-           "arm_joint1": -3.1361150423869044,
-           "arm_joint2": 0.9799041287896845,
-           "arm_joint3": 0.036062767152477006,
-           "arm_joint4": -0.60224226913142,
-           "arm_joint5": 0.005872011547052069,
-           "arm_joint6": 1.5410355751365572,
-           "arm_joint7": -1.5180754271989076,
+            "arm_joint1": -3.1361150423869044,
+            "arm_joint2": 0.9799041287896845,
+            "arm_joint3": 0.036062767152477006,
+            "arm_joint4": -0.60224226913142,
+            "arm_joint5": 0.005872011547052069,
+            "arm_joint6": 1.5410355751365572,
+            "arm_joint7": -1.5180754271989076,
         }
         gain = {
-           "arm_joint1": 1,
-           "arm_joint2": -1,
-           "arm_joint3": 1,
-           "arm_joint4": -1,
-           "arm_joint5": 1,
-           "arm_joint6": 1,
-           "arm_joint7": -1,
+            "arm_joint1": 1,
+            "arm_joint2": -1,
+            "arm_joint3": 1,
+            "arm_joint4": -1,
+            "arm_joint5": 1,
+            "arm_joint6": 1,
+            "arm_joint7": -1,
         }
-        out = {remap[k]: v*gain[k]+off[k] for k, v in data.items()}
+        out = {remap[k]: v * gain[k] + off[k] for k, v in data.items()}
         return out
 
     @error_catcher
     async def main(self):
-        await self.update()
-        for n in range(len(self.path)):
-            print(f"{n=}")
-            target = self.step(n)
-            await rao.wait_for(self, self.syncer.asap(target), timeout_sec=100)
+        await self.ready_up()
+        for step in range(len(self.JSON_TRAJECTORY)):
+            print(f"{step=}")
+            target = self.json_step(step)
+            await rao.wait_for(self, self.syncer.lerp(target), timeout_sec=100)
 
         print("finished")
 
     @error_catcher
-    def start(self):
+    def startup(self):
         rao.ensure_future(self, self.main())
-        # self.executor.create_task(self.update())
         self.destroy_timer(self.startTMR)
-        print("destroyed start")
+        print("Startup done.")
 
     @error_catcher
     def loop(self):
