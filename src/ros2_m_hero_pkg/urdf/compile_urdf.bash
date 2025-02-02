3dfa144a6fae5f8f71af8a965be948749068a73d
elian-WSL22H <elian.neppel@posteo.eu>
2025-02-03 00:39:12 +0900

    urdf minifix and deleted old

--- src/ros2_m_hero_pkg/urdf/compile_urdf.bash
+++ src/ros2_m_hero_pkg/urdf/compile_urdf.bash
@@ -3,7 +3,7 @@
 source /opt/ros/humble/setup.sh
 xacro ./hero_7dof.xacro number:=1 > ./hero_7dof1.urdf
 xacro ./hero_minimal.xacro limb:=1 wheel:=1 > ./hero_minimal.urdf
-xacro ./hero_wheel_module.xacro number:=11 > ./hero_wheel_module.urdf
+xacro ./hero_wheel.xacro number:=11 > ./hero_wheel_module.urdf
 xacro ./hero_vehicle.xacro > ./hero_vehicle.urdf
 xacro ./hero_dragon.xacro > ./hero_dragon.urdf
 xacro ./hero_tricycle.xacro > ./hero_tricycle.urdf
