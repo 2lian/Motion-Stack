2d2eac89604a3424845b33366328032b09585eb1
elian-WSL22H <elian.neppel@posteo.eu>
2025-02-09 13:07:15 +0900

    ik joint api doc update

--- src/motion_stack/motion_stack/api/ik_syncer.py
+++ src/motion_stack/motion_stack/api/ik_syncer.py
@@ -3,23 +3,14 @@ Python API to sync the movement of several end_effectors.
 This requires ik lvl2 to be running.
 
 Note:
-    The ros2 implementation is available in :py:class:`.ros2.ik_api`.
+    The ros2 implementation is available in :py:mod:`.ros2.ik_api`.
 
 This high level API alows for multi-end-effector control and syncronization (over several legs). This is the base class where, receiving and sending data to motion stack lvl2 is left to be implemented.
 """
 
+import warnings
 from abc import ABC, abstractmethod
-from typing import (
-    Awaitable,
-    Callable,
-    Dict,
-    Generic,
-    List,
-    NamedTuple,
-    Set,
-    TypeVar,
-    Union,
-)
+from typing import Awaitable, Callable, Dict, List, Set
 
 import nptyping as nt
 import numpy as np
@@ -27,12 +18,7 @@ from nptyping import NDArray, Shape
 
 from motion_stack.core.utils.time import Time
 
-from ..core.utils.hypersphere_clamp import (
-    clamp_multi_xyz_quat,
-    clamp_to_sqewed_hs,
-    clamp_xyz_quat,
-    fuse_xyz_quat,
-)
+from ..core.utils.hypersphere_clamp import clamp_multi_xyz_quat
 from ..core.utils.joint_state import JState
 from ..core.utils.math import Flo3, Quaternion, qt
 from ..core.utils.pose import Pose, XyzQuat
@@ -47,7 +33,37 @@ LimbNumber = int
 MultiPose = Dict[LimbNumber, Pose]
 
 
+class SensorSyncWarning(Warning):
+    pass
+
+
 class IkSyncer(ABC):
+    r"""One instance controls and syncronises several limbs end-effectors, safely executing trajectory to targets.
+
+    Note:
+        This class is an abstract base class, the ros2 implementation is available in :py:class:`.ros2.joint_api.IkSyncerRos`. Hence,  parts of this class are left to be implmented by the interface/runtime: \ :py:meth:`.IkSyncer.FutureT`, :py:meth:`.IkSyncer.sensor`, :py:meth:`.IkSyncer.send_to_lvl2`.
+
+    Important:
+        \ :py:meth:`.IkSyncer.execute` must be called to compute, update and send the command.
+
+        One object instance can only execute one target at a time. However, the limbs or targets can change between calls of the same instance, before the previous task is done.
+
+    The trajectory interpolates between two points:
+
+        - The last position (if None: uses sensor, else: last sub-target). This is handled automatically, however ``clear`` resets the last position to None.
+        - The input target.
+
+    Several interpolation strategies are available:
+
+     - LERP: :py:meth:`.IkSyncer.lerp`
+     - ASAP: :py:meth:`.IkSyncer.asap`
+     - Unsafe: :py:meth:`.IkSyncer.unsafe`
+
+    Args:
+        interpolation_delta: (mm, rad) During movement, how much divergence is allowed from the path. If exceeded, movement slows down.
+        on_target_delta: (mm, rad) Delta at which the trajectory/task is considered finished and the Future is switched to ``done``.
+    """
+
     _COMMAND_DONE_DELTA: XyzQuat[float, float] = XyzQuat(0.01, np.deg2rad(0.01))
 
     def __init__(
@@ -69,11 +85,23 @@ class IkSyncer(ABC):
         This must be called frequently."""
         self._trajectory_task()
 
+    def clear(self):
+        """Resets the trajectory starting point onto the current sensor positions.
+
+        Important:
+            Use when:
+                - The trajectory is stuck unable to interpolate.
+                - External motion happened, thus the last position used by the syncer is no longer valid.
+
+        Caution:
+            At task creation, if the syncer applies `clear()` automatically, `SensorSyncWarning` is issued. Raise the warning as an error to interupt operation if needed.
+        """
+        self._previous = {}
+
     def lerp(self, target: MultiPose) -> FutureType:
         """Starts executing a lerp trajectory toward the target.
 
-        Args:
-            target: key = joint name ; value = joint angle
+        LERP: all joints reach the target at the same time.
 
         Returns:
             Future of the task. Done when sensors are on target.
@@ -83,39 +111,61 @@ class IkSyncer(ABC):
     def asap(self, target: MultiPose) -> FutureType:
         """Starts executing a asap trajectory toward the target.
 
-        Args:
-            target: key = joint name ; value = joint angle
+        ASAP: (Not Implemented) joints will reach their tagets indepently, as fast as possible.
 
         Returns:
-            Future of the task. Done when sensorare on target.
+            Future of the task. Done when sensors are on target.
         """
         return self._make_motion(target, self.asap_toward)
 
     def unsafe(self, target: MultiPose) -> FutureType:
         """Starts executing a unsafe trajectory toward the target.
 
-        Args:
-            target: key = joint name ; value = joint angle
+        Unsafe: Similar to ASAP except the final target is sent directly to the IK, so the movement will not stop in case of crash, errors, network issue AND you cannot cancel it.
 
         Returns:
-            Future of the task. Done when sensorare on target.
+            Future of the task. Done when sensors are on target.
         """
         return self._make_motion(target, self.unsafe_toward)
 
+    def abs_from_rel(self, offset: MultiPose) -> MultiPose:
+        """Absolute position of the MultiPose that corresponds to the given relative offset.
+
+        Example:
+            *joint_1* is at 45 deg, offset is 20 deg. Return will be 65 deg.
+
+        Args:
+            offset: Relative postion.
+
+        Returns:
+            Absolute position.
+        """
+        track = set(offset.keys())
+        prev = self._previous_point(track)
+        return {
+            key: Pose(
+                offset[key].time,
+                prev[key].xyz + offset[key].xyz,
+                prev[key].quat * offset[key].quat,
+            )
+            for key in track
+        }
+
+
     @abstractmethod
-    def send_to_lvl1(self, ee_targets: MultiPose):
+    def send_to_lvl2(self, ee_targets: MultiPose):
         """Sends ik command to lvl2.
 
         Important:
             This method must be implemented by the runtime/interface.
 
         Note:
-            Default ROS2 implementation: :py:meth:`.ros2.ik_api.IkSyncerRos.send_to_lvl1`
+            Default ROS2 implementation: :py:meth:`.ros2.ik_api.IkSyncerRos.send_to_lvl2`
 
         Args:
             states: Ik target to be sent to lvl1
         """
-        pass
+        ...
 
     @property
     @abstractmethod
@@ -133,29 +183,6 @@ class IkSyncer(ABC):
         """
         ...
 
-    def abs_from_offset(self, offset: MultiPose) -> MultiPose:
-        """Absolute position of the joints that correspond to the given relative offset.
-
-        Example:
-            *joint_1* is at 45 deg, offset is 20 deg. Return will be 65 deg.
-
-        Args:
-            offset: Offset dictionary, keys are the joint names, value the offset in rad.
-
-        Returns:
-            Absolute position.
-        """
-        track = set(offset.keys())
-        prev = self._previous_point(track)
-        return {
-            key: Pose(
-                offset[key].time,
-                prev[key].xyz + offset[key].xyz,
-                prev[key].quat * offset[key].quat,
-            )
-            for key in track
-        }
-
     @property
     @abstractmethod
     def sensor(self) -> MultiPose:
@@ -171,13 +198,6 @@ class IkSyncer(ABC):
 
         """
         ...
-
-    def clear(self):
-        """Resets the trajectory starting point onto the current sensor positions.
-
-        Mainly usefull when the trajectory is stuck because it could not reach a target.
-        """
-        self._previous = {}
 
     def _previous_point(self, track: set[LimbNumber]) -> MultiPose:
         """
@@ -232,11 +252,11 @@ class IkSyncer(ABC):
 
     def _get_asap_step(self, target: MultiPose) -> MultiPose:
         """Result of a single step of asap."""
-        ...
+        return NotImplemented
 
     def _get_unsafe_step(self, target: MultiPose) -> MultiPose:
         """Result of a single step of unsafe."""
-        ...
+        return target
 
     def _traj_toward(
         self,
@@ -252,7 +272,7 @@ class IkSyncer(ABC):
 
         if not command_done:
             next = step_func(target)
-            self.send_to_lvl1(next)
+            self.send_to_lvl2(next)
             self._update_previous_point(next)
 
         else:
@@ -269,9 +289,6 @@ class IkSyncer(ABC):
     def unsafe_toward(self, target: MultiPose) -> bool:
         """Executes one single unsafe step.
 
-        Args:
-            target: key = joint name ; value = joint angle
-
         Returns:
             True if trajectory finished
         """
@@ -280,9 +297,6 @@ class IkSyncer(ABC):
     def asap_toward(self, target: MultiPose) -> bool:
         """Executes one single asap step.
 
-        Args:
-            target: key = joint name ; value = joint angle
-
         Returns:
             True if trajectory finished
         """
@@ -290,9 +304,6 @@ class IkSyncer(ABC):
 
     def lerp_toward(self, target: MultiPose) -> bool:
         """Executes one single lerp step.
-
-        Args:
-            target: key = joint name ; value = joint angle
 
         Returns:
             True if trajectory finished
@@ -312,6 +323,16 @@ class IkSyncer(ABC):
             Future of the task. Done when sensorare on target.
         """
         future = self.FutureT()
+
+        track = set(target.keys())
+        prev, center = self._previous_and_center(track)
+        if not _multipose_close(track, prev, center, atol=self._interpolation_delta):
+            warnings.warn(
+                "Syncer is out of sync with sensor data. Calling `syncer.clear()` to reset the syncer onto the sensor position. Raise this warning as an error to interupt operations.",
+                SensorSyncWarning,
+                stacklevel=3,
+            )
+            self.clear()
 
         def step_toward_target():
             if future.cancelled():
