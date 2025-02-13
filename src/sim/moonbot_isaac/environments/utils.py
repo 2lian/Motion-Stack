b2a8bac3af9bf4050d2e1efb018f2db6534d2aff
Andras Polgar <azazdeaz@gmail.com>
2025-02-13 19:15:48 +0900

    Fix setting initial positions

--- src/sim/moonbot_isaac/environments/utils.py
+++ src/sim/moonbot_isaac/environments/utils.py
@@ -1,9 +1,12 @@
 import logging
 
+import pxr
 from omni.isaac.core.utils.prims import get_prim_at_path
+from pxr import Gf
 
 from environments.config import TransformConfig
-from pxr import Gf
+
+# import omni.kit.property.usd.prim_selection_payload.PrimSelectionPayload
 
 
 
@@ -21,6 +24,22 @@ def set_attr(prim, attr_name, value):
         logging.warn(f"Attribute {attr_name} not found in {prim.GetPath()}")
 
 def apply_transform_config(prim, transform: TransformConfig):
+    if type(prim) is str:
+        prim = get_prim_at_path(prim)
+
+    prop_names = prim.GetPropertyNames()
+    xformable = pxr.UsdGeom.Xformable(prim)
+    xformable.ClearXformOpOrder()
+    if "xformOp:translate" not in prop_names:
+        xformable.AddXformOp(
+            pxr.UsdGeom.XformOp.TypeTranslate, pxr.UsdGeom.XformOp.PrecisionDouble, ""
+        )
+
+    if "xformOp:orient" not in prop_names:
+        xformable.AddXformOp(
+            pxr.UsdGeom.XformOp.TypeOrient, pxr.UsdGeom.XformOp.PrecisionDouble, ""
+        )
+    
     set_attr(
         prim,
         "xformOp:translate",
@@ -40,5 +59,3 @@ def apply_transform_config(prim, transform: TransformConfig):
             transform.rotation[3],
         ),
     )
-    
-
