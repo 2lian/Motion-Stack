4eabc124b2e74b52a259115e85d95c8d8ccc3437
Andras Polgar <azazdeaz@gmail.com>
2025-02-17 17:11:11 +0900

    Track all TF transforms with one ROS node

--- src/sim/moonbot_isaac/moonbot_isaac/tf_transform_monitor.py
+++ src/sim/moonbot_isaac/moonbot_isaac/tf_transform_monitor.py
@@ -7,59 +7,51 @@ from geometry_msgs.msg import TransformStamped
 from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
+import yaml
 
 
 class TfTransformMonitor(Node):
     """
-    Continously querying the transform between two frames (Used in the Isaac Sim environment)
+    Track the relative transform from fixed_frame for all connected frames
     """
 
     def __init__(
         self,
-        frame_id: str = "world",
-        child_frame_id: str = "gt__wheel11_body",
+        fixed_frame: str = "world",
         check_period_sec=0.04,
         queue: Optional[Queue] = None,
     ):
-        super().__init__(f"tf_transform_monitor_from_{frame_id}_to_{child_frame_id}")
+        super().__init__(f"tf_transform_monitor_from_{fixed_frame}")
 
-        self.latest_transform = None
-        self.frame_id = frame_id
-        self.child_frame_id = child_frame_id
+        self.fixed_frame = fixed_frame
         self.queue = queue
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.all_frames = set()
 
         self.timer = self.create_timer(check_period_sec, self.on_timer)
-        self.error_log_period = 1.0
-        self.last_error_log_time = None
+
+    def remove_prim_from_articulation(self, prim_path: str):
+        pass
 
     def on_timer(self):
-        try:
-            transform = self.tf_buffer.lookup_transform(
-                source_frame=self.frame_id,
-                target_frame=self.child_frame_id,
-                time=rclpy.time.Time(seconds=0),  # Use latest available transform
-            )
-            self.latest_transform = transform
+        frames = self.tf_buffer.all_frames_as_yaml()
+        frames = yaml.safe_load(frames)
+        frames = set(frames.keys())
 
-            if self.queue is not None:
-                self.queue.put(transform)
-        except TransformException as e:
-            # Limit how often we log the error and increase the period exponentially
-            if (
-                self.last_error_log_time is None
-                or (self.get_clock().now() - self.last_error_log_time).nanoseconds
-                > self.error_log_period * 1e9
-            ):
-                self.last_error_log_time = self.get_clock().now()
-                self.error_log_period *= 2
-                self.get_logger().error(
-                    f"Could not get transform between {self.frame_id} and {self.child_frame_id}: {e}"
+        for frame in frames:
+            try:
+                transform = self.tf_buffer.lookup_transform(
+                    source_frame=frame,
+                    target_frame=self.fixed_frame,
+                    time=rclpy.time.Time(seconds=0),  # Use latest available transform
                 )
+                self.latest_transform = transform
 
-    def get_latest_transform(self) -> Optional[TransformStamped]:
-        return self.latest_transform
+                if self.queue is not None:
+                    self.queue.put(transform)
+            except TransformException as _:
+                pass
 
 
 def main(args=None):
