6d7f7c8f5f59347d78847429fa2c8d1d09762652
Andras Polgar <azazdeaz@gmail.com>
2025-02-26 12:36:59 +0900

    Remove duplicated code

--- src/sim/moonbot_isaac/environments/robot_definition_reader.py
+++ src/sim/moonbot_isaac/environments/robot_definition_reader.py
@@ -9,30 +9,9 @@ from typing import Any, List
 from xml.dom.minidom import Node as DomNode
 from xml.dom.minidom import parseString
 
-from ament_index_python.packages import get_package_share_directory
-
 from environments.config import RobotConfig
 from environments.isaac_utils import set_attr_cmd
-
-
-def replace_package_urls_with_paths(input_string):
-    # Define the regex pattern to match substrings starting with "package://" and ending with a quote mark
-    pattern = r"package://([^/]+)"  # Capturing group to extract package name
-
-    # Find all matches of the pattern in the input string
-    matches = re.findall(pattern, input_string)
-
-    # Iterate through matches and replace package URLs with file paths
-    for package_name in matches:
-        try:
-            package_path = get_package_share_directory(package_name)
-            package_url = "package://" + package_name
-            input_string = input_string.replace(package_url, package_path)
-        except Exception as e:
-            logging.error(f"Error while replacing package URL with path: {e}")
-
-    return input_string
-
+from environments.ros_utils import replace_package_urls_with_paths
 
 @dataclass
 class IsaacAttribute:
