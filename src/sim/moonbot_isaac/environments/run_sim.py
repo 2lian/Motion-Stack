513f32c3fbc01978da270d4c003d6ab89ffce783
Andras Polgar <azazdeaz@gmail.com>
2025-06-17 15:17:24 +0200

    Test setting initial joint positions

--- src/sim/moonbot_isaac/environments/run_sim.py
+++ src/sim/moonbot_isaac/environments/run_sim.py
@@ -5,6 +5,8 @@ import os
 import sys
 from pathlib import Path
 
+import numpy as np
+
 # Add the package root to sys.path
 package_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(package_root)
@@ -25,7 +27,6 @@ from environments.config import SimConfig, load_config
 logging.info(f"Loading sim config from {sim_config_path}")
 config: SimConfig = load_config(sim_config_path)
 pprint(config.model_dump())
-
 
 
 os.environ["OMNI_KIT_ACCEPT_EULA"] = "YES"
@@ -55,12 +56,13 @@ def reference_usd(usd_file: str, prim_path: str):
     path = str(Path(__file__).parent / "usd" / usd_file)
     return add_reference_to_stage(usd_path=path, prim_path=prim_path)
 
-from environments.load_moonbot import load_moonbot
-from environments.realsense_camera import RealsenseCamera
-from environments.isaac_utils import apply_transform_config
+
 from environments.ground_truth_tf import GroundTruthTF
+from environments.isaac_utils import apply_transform_config
 from environments.joint_controller import JointController
+from environments.load_moonbot import load_moonbot
 from environments.observer_camera import ObserverCamera
+from environments.realsense_camera import RealsenseCamera
 
 world = World(stage_units_in_meters=1.0)
 world.play()
@@ -74,7 +76,7 @@ for robot in config.robots:
     if not robot.visualization_mode:
         if robot.publish_ground_truth_tf:
             GroundTruthTF(robot_prim=robot_path).initialize()
-        
+
         if not robot.without_controls:
             JointController(robot_prim=robot_path).initialize()
 
@@ -107,11 +109,54 @@ omni.kit.commands.execute(
     select_new_prim=True,
 )
 
+
+def set_initial_joint_positions():
+    from omni.isaac.core.articulations import Articulation
+    from omni.isaac.dynamic_control import _dynamic_control
+
+    dc = _dynamic_control.acquire_dynamic_control_interface()
+    art = dc.get_articulation("/hero_arm")
+    articulation = Articulation("/hero_arm")
+    positions = [
+        ("leg1grip1", -0.026),
+        ("leg1joint1", np.pi / 2),
+        ("leg1joint2", -np.pi / 2),
+        ("leg1joint4", np.pi / 2),
+        ("leg1joint6", np.pi / 2),
+        ("leg1joint7", np.pi),
+    ]
+    for dof_name in articulation.dof_names:
+        for position in positions:
+            if dof_name == position[0]:
+                logging.info(f"Articulation DOF: {dof_name}")
+                dof_ptr = dc.find_articulation_dof(art, dof_name)
+                if dof_ptr > 0:
+                    dc.set_dof_position(dof_ptr, position[1])
+                    dc.set_dof_position_target(
+                        dof_ptr,
+                        position[1],
+                    )
+
+
 world.reset()
+is_first_step = True
+
+# Start and stop the simulation. For some reason, IsaacSim is unstable at first when the intial joint positions are set.
+world.step(render=True)
+set_initial_joint_positions()
+world.step(render=True)
+world.reset()
+
 
 while simulation_app.is_running():
     world.step(render=True)
 
     if world.is_playing():
-        pass
+        if is_first_step:
+            # Set initial joint positions only once at the start of the simulation
+            # TODO: This should run after world the world reset. Probably possible with a callback.
+            set_initial_joint_positions()
+            is_first_step = False
+    else:
+        is_first_step = True
 simulation_app.close()
