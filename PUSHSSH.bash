f4f313573f1371b4c779928dab5beadf7251d0f1
Neppel Elian <elian.neppel@posteo.eu>
2025-07-24 17:54:39 +0900

    added v3

--- PUSHSSH.bash
+++ PUSHSSH.bash
@@ -5,11 +5,12 @@ source ~/.bashrc
 export RCUTILS_CONSOLE_OUTPUT_FORMAT="{message}"
 export RCUTILS_COLORIZED_OUTPUT=1
 
-SSH_ADDRESS=hero-arm-v1-1
+# SSH_ADDRESS=hero-arm-v1-1
+SSH_ADDRESS=moonshot@192.168.8.130
 SOURCE_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-DESTINATION_DIR="${SSH_ADDRESS}:~/moonbot_tools/cache/"
+DESTINATION_DIR="${SSH_ADDRESS}:~/moonbot_tools/cache/motion_stack/"
 
 # rsync -av --checksum --progress --exclude='build/'--exclude='install/'  --exclude='log/' --exclude='*cache*' $SOURCE_DIR $DESTINATION_DIR
 
-rsync -av --checksum --progress --exclude='*cache*' --exclude='.*' --exclude='hero_workspace' --include='***' $SOURCE_DIR $DESTINATION_DIR
+rsync -av --checksum --progress --exclude='*cache*' --exclude='.*' --include='***' $SOURCE_DIR $DESTINATION_DIR
 
