6bad19743e0ec6e84de8fab51abeb2a5c4e622a1
Elian NEPPEL <elian.neppel@posteo.eu>
2025-08-24 01:41:05 +0900

    fixed new patching

--- src/motion_stack/motion_stack/core/rtb_fix/patch.py
+++ src/motion_stack/motion_stack/core/rtb_fix/patch.py
@@ -2,7 +2,7 @@ def patch():
     """Applies the patch to rtb"""
     import roboticstoolbox.tools.urdf.urdf as bad
 
-    import fixed_urdf as fix
+    import motion_stack.core.rtb_fix.fixed_urdf as fix
 
     bad.URDF.__init__ = fix.URDF.__init__
     bad.URDF._recursive_axis_definition = fix.URDF._recursive_axis_definition
